a87598819ef4cddda5ad67250160115daac5331a
Gabriel Rocheleau <contact@rockwaterweb.com>
2021-07-29 15:32:10 -0400

    fix: bytes32 instead of bytes

--- packages/ethers/src/contracts/IERC1271.ts
+++ packages/ethers/src/contracts/IERC1271.ts
@@ -8,7 +8,7 @@ interface IERC1271 extends Contract<IERC1271> {
   }
 
 export const IERC1271 = contract<IERC1271>()`
-function isValidSignature(bytes _hash, bytes _signature) view returns (bytes4)
+function isValidSignature(bytes32 _hash, bytes _signature) view returns (bytes4)
   `;
 
 // Returns 0x1626ba7e if signature is valid
