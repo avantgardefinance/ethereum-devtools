5add97c9eae408602711a1574c5889e721b1ef22
Gabriel Rocheleau <contact@rockwaterweb.com>
2021-07-29 13:21:19 -0400

    chore: provider as prop

--- packages/ethers/src/utils/verifySignature.ts
+++ packages/ethers/src/utils/verifySignature.ts
@@ -1,4 +1,4 @@
-import { BigNumber, utils } from 'ethers';
+import { providers, utils } from 'ethers';
 
 import { IERC1271 } from '../contracts/IERC1271';
 import { sameAddress } from './sameAddress';
@@ -7,9 +7,15 @@ interface VerifySignatureProps {
   walletAddress: string;
   message: string | utils.Bytes;
   signature: string;
+  provider: providers.StaticJsonRpcProvider;
 }
 
-export async function verifySignature({ walletAddress, message, signature }: VerifySignatureProps): Promise<boolean> {
+export async function verifySignature({
+  walletAddress,
+  message,
+  signature,
+  provider,
+}: VerifySignatureProps): Promise<boolean> {
   try {
     const bytecode = await provider.getCode(walletAddress);
     const isSmartContract = bytecode && utils.hexStripZeros(bytecode) !== '0x';
@@ -18,7 +24,7 @@ export async function verifySignature({ walletAddress, message, signature }: Ver
       const contract = new IERC1271(walletAddress, provider);
       const result = await contract.isValidSignature(hash, signature);
       // Per https://eips.ethereum.org/EIPS/eip-1271
-      return result === BigNumber.from(0x1626ba7e);
+      return result.eq(0x1626ba7e);
     } else {
       const recoveredAddress = utils.verifyMessage(message, signature);
       return sameAddress(recoveredAddress, walletAddress);
