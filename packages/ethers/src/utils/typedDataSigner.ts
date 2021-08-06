45c9d53a865fab49d9585a4ebc6cd12bae477814
Gabriel Rocheleau <contact@rockwaterweb.com>
2021-08-06 11:09:11 -0400

    feat: typed data interface

--- packages/ethers/src/utils/typedDataSigner.ts
+++ packages/ethers/src/utils/typedDataSigner.ts
@@ -1,8 +1,14 @@
-import type { TypedDataSigner } from '@ethersproject/abstract-signer';
+import type { TypedDataDomain, TypedDataField, TypedDataSigner } from '@ethersproject/abstract-signer';
 import type { Signer } from 'ethers';
 
 export interface SignerWithTypedDataSupport extends Signer, TypedDataSigner {}
 
 export function isTypedDataSigner(signer: Signer): signer is SignerWithTypedDataSupport {
   return typeof (signer as any)._signTypedData === 'function';
+}
+
+export interface TypedData {
+  domain: TypedDataDomain;
+  types: { [key: string]: TypedDataField[] };
+  data: Record<string, any>;
 }
