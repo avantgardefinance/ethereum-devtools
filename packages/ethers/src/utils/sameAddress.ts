911492c77367b4804c04cf52027dcca3947188fb
Fran <f.x.rodriguez@hotmail.com>
2022-01-17 14:05:31 +0100

    fix: remove comment

--- packages/ethers/src/utils/sameAddress.ts
+++ packages/ethers/src/utils/sameAddress.ts
@@ -7,7 +7,6 @@ export function sameAddress(a: AddressLike, b: AddressLike) {
 
 export function safeSameAddress(a: AddressLike | null | undefined, b: AddressLike | null | undefined) {
   try {
-    // Potentially also do more checks on the inputs and not just for truthyness?
     return a && b && sameAddress(a, b);
   } catch {}
 
