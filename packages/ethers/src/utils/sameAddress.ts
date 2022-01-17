c9736989c8d95a612e799fc628832f4eea37b2b9
Fran <f.x.rodriguez@hotmail.com>
2022-01-17 13:33:51 +0100

    feat: sameAddress util

--- packages/ethers/src/utils/sameAddress.ts
+++ packages/ethers/src/utils/sameAddress.ts
@@ -4,3 +4,12 @@ import { resolveAddress } from './resolveAddress';
 export function sameAddress(a: AddressLike, b: AddressLike) {
   return resolveAddress(a) === resolveAddress(b);
 }
+
+export function safeSameAddress(a: AddressLike | null | undefined, b: AddressLike | null | undefined) {
+  try {
+    // Potentially also do more checks on the inputs and not just for truthyness?
+    return a && b && sameAddress(a, b);
+  } catch {}
+
+  return false;
+}
