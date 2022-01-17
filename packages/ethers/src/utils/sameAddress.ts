6dcfcd0f861071f387fec834e50427fcfe91cda6
Fran Hodl <f.x.rodriguez@hotmail.com>
2022-01-17 13:07:16 +0000

    feat: safeSameAddress util (#59)
    feat: safeSameAddress util

--- packages/ethers/src/utils/sameAddress.ts
+++ packages/ethers/src/utils/sameAddress.ts
@@ -4,3 +4,11 @@ import { resolveAddress } from './resolveAddress';
 export function sameAddress(a: AddressLike, b: AddressLike) {
   return resolveAddress(a) === resolveAddress(b);
 }
+
+export function safeSameAddress(a: AddressLike | null | undefined, b: AddressLike | null | undefined) {
+  try {
+    return a && b && sameAddress(a, b);
+  } catch {}
+
+  return false;
+}
